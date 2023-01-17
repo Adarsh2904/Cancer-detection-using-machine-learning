cda976eb249fca9d19a3af698bf9cc23cab7faa2
Adarsh2904 <109025700+Adarsh2904@users.noreply.github.com>
2023-01-17 23:20:00 +0530

    Add files via upload

---
+++ testofmodel.py
@@ -0,0 +1,17 @@
+import cv2
+from keras.models import load_model
+from PIL import Image
+import os
+import numpy as np
+
+model=load_model('Braintumor1st2nd.h5')
+image=cv2.imread('C:\\Project clg\\pred\\pred0.jpg')
+img=Image.fromarray(image)
+img=img.resize((64,64))
+
+img = np.array(img)
+print(img)
+#if all as zero no cancer or it is cancer
+
+
+
